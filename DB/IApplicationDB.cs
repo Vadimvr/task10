63869a88eae438cf79a4bd47a5576d59bd7f6609
Vadzim <40643436+Vadimvr@users.noreply.github.com>
2023-10-10 20:20:35 +0300

    Updated  Db

---
+++ DB/IApplicationDB.cs
@@ -0,0 +1,31 @@
+﻿using Models;
+using System.Collections.Generic;
+
+namespace DB
+{
+    public interface IApplicationDB
+    {
+        IEntitiesDB<Account, int> Accounts { get; }
+        IEntitiesDB<Mode, int> Modes { get; }
+        IEntitiesDB<Step, int> Steps { get; }
+    }
+
+    public class MemoryDB : IApplicationDB
+    {
+
+        EntitiesDB<Account, int> accounts = null;
+        EntitiesDB<Mode, int> modes = null;
+        EntitiesDB<Step, int> steps = null;
+
+        public MemoryDB()
+        {
+            accounts = new EntitiesDB<Account, int>(new List<Account>());
+            modes = new EntitiesDB<Mode, int>(new List<Mode>());
+            steps = new EntitiesDB<Step, int>(new List<Step>());
+        }
+
+        public IEntitiesDB<Account, int> Accounts { get => accounts; }
+        public IEntitiesDB<Mode, int> Modes { get => modes; }
+        public IEntitiesDB<Step, int> Steps { get => steps; }
+    }
+}
---
+++ DB/IEntitiesDB.cs
@@ -0,0 +1,40 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace DB
+{
+    public interface IEntitiesDB<T, I> where T : IEntity<I> where I : struct
+    {
+        T Get(I id);
+        List<T> GetAll();
+        void Update(T t);
+        void Delete(T t);
+        void Add(T t);
+    }
+
+    public class EntitiesDB<T, I> : IEntitiesDB<T, I> where T : IEntity<I> where I : struct
+    {
+        private List<T> entities;
+
+        public EntitiesDB(List<T> arr) { entities = arr; }
+        public void Add(T t) => entities.Add(t);
+        public void Delete(T t) => entities.Remove(t);
+        public T Get(I id) => entities.FirstOrDefault(i => i.ID.Equals(id));
+        public List<T> GetAll() => entities;
+        public void Update(T t)
+        {
+            T item = default(T);
+            int i = 0;
+            for (; i < entities.Count; i++)
+            {
+                item = entities.First(e => e.ID.Equals(t.ID));
+            }
+            if (item == null) throw new ArgumentNullException();
+
+            entities[i] = t;
+        }
+    }
+}
