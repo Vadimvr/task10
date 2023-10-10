05fe9d9b11b28f71aba1ae8d33597cf050d97bfa
Vadzim <40643436+Vadimvr@users.noreply.github.com>
2023-10-10 20:20:10 +0300

    Updated models

--- Models/Mode.cs
+++ Models/Mode.cs
@@ -1,31 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Models
+﻿namespace Models
 {
-    public class Mode
+    public interface IEntity<I> where I : struct
+    {
+        I ID { get; set; }
+    }
+
+    public class Mode : IEntity<int>
     {
         public int ID { get; set; }
         public string Name { get; set; }
         public int MaxBottleNumber { get; set; }
         public int MaxUsedTips { get; set; }
     }
-    public class Step
+    public class Step : IEntity<int>
     {
         public int ID { get; set; }
         public Mode Mode { get; set; }
         public int ModeID { get; set; }
-               
+
         public int Timer { get; set; }
         public int Speed { get; set; }
         public string Type { get; set; }
         public int Volume { get; set; }
     }
 
-    public class Account
+    public class Account : IEntity<int>
     {
         public int ID { get; set; }
         public string Email { get; set; }
