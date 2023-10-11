0d169ef47444ef4f12e6c0570989e6385ac72fb2
Vadzim <40643436+Vadimvr@users.noreply.github.com>
2023-10-11 11:42:34 +0300

    Added global message box

---
+++ MessageBox/MessageBox/MessageServic.cs
@@ -0,0 +1,15 @@
+﻿namespace Presenter.MessageBox
+{
+    public interface IMessageService
+    {
+        void Show(string message);
+    }
+
+    public class MessageService : IMessageService
+    {
+        public void Show(string message)
+        {
+            System.Windows.Forms.MessageBox.Show(message);
+        }
+    }
+}
