5d70656eacf7c7296dd45b8350e9f190b090780d
Vadzim <40643436+Vadimvr@users.noreply.github.com>
2023-10-11 11:47:27 +0300

    Updated entities in the database

--- DB/IEntitiesDB.cs
+++ DB/IEntitiesDB.cs
@@ -1,8 +1,7 @@
 ﻿using Models;
-using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace DB
 {
@@ -11,30 +10,7 @@ namespace DB
         T Get(I id);
         List<T> GetAll();
         void Update(T t);
-        void Delete(T t);
+        void Delete(I id);
         void Add(T t);
-    }
-
-    public class EntitiesDB<T, I> : IEntitiesDB<T, I> where T : IEntity<I> where I : struct
-    {
-        private List<T> entities;
-
-        public EntitiesDB(List<T> arr) { entities = arr; }
-        public void Add(T t) => entities.Add(t);
-        public void Delete(T t) => entities.Remove(t);
-        public T Get(I id) => entities.FirstOrDefault(i => i.ID.Equals(id));
-        public List<T> GetAll() => entities;
-        public void Update(T t)
-        {
-            T item = default(T);
-            int i = 0;
-            for (; i < entities.Count; i++)
-            {
-                item = entities.First(e => e.ID.Equals(t.ID));
-            }
-            if (item == null) throw new ArgumentNullException();
-
-            entities[i] = t;
-        }
     }
 }
